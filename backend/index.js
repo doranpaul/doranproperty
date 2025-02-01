6f9be4fcb2e04482805ae84ddc7bf3b44780cb09
40070680 <pdoran12@qub.ac.uk>
2025-02-01 13:17:30 +0000

    changes made to index.js and to allow server to run on render and also for dev environment

--- backend/index.js
+++ backend/index.js
@@ -70,6 +70,6 @@ app.post('/send-message', async (req, res) => {
 
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
